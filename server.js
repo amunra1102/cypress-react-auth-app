6bb2a6f5d8e90fd740798dfa406ded397a2e101a
Thanh Van Vu <thanh.vu@hitachivantara.com>
2021-08-22 13:27:31 +0700

    initial and config project

---
+++ server.js
@@ -0,0 +1,64 @@
+const jsonServer = require('json-server');
+const auth = require('basic-auth');
+const jwt = require('express-jwt');
+const jwtBase = require('jsonwebtoken');
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middleware = jsonServer.defaults();
+
+const secret = 'a26533d9-95e2-498f-9c9c-bdb48fa98471';
+
+// Set default middleware
+server.use(middleware);
+
+// Route to issue JWT when a user logs in with basic authentication
+server.get('/user_token', (req, res) => {
+  const user = auth(req);
+  const db = router.db;
+  const users = db.get('users').value();
+  // This is a naive way to validate username and password, the password should be hashed
+  const foundUser = users.find((u) => u.username === user.name && u.password === user.pass);
+  if (foundUser) {
+    const token = jwtBase.sign({
+      id: foundUser.id,
+      username: foundUser.username,
+      role: foundUser.role
+    }, secret);
+    res.json({
+      username: foundUser.username,
+      role: foundUser.role,
+      token: token
+    });
+  } else res.status(401).json({ error: 'Incorrect credentials' });
+});
+
+// Require user to be logged in to hit server routes
+// Make user_token route available to anonymous users
+server.use(jwt({ secret, algorithms: ['RS256'] })
+  .unless({
+    path: [
+      '/user_token'
+    ]
+  }));
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser);
+
+// Restrict access to routes based on role
+server.use((req, res, next) => {
+  let userHasPermission = true;
+  if (req.method === 'POST' || req.method === 'DELETE') {
+    // Only admin can add or delete courses
+    if (req.path === '/courses' && req.user.role !== 'admin') {
+      userHasPermission = false;
+    }
+  }
+  if (userHasPermission) next();
+  else res.status(403).json({ error: 'No permission' });
+});
+
+// Use default router
+server.use(router);
+server.listen(3001, () => console.log('JSON Server is running'));
